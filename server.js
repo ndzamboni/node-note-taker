6e4cd7a74dfcc2b56293c56360612c21526b92b3
ndzamboni <ndzamboni@gmail.com>
2024-06-15 13:21:45 -0400

    making progress

--- server.js
+++ server.js
@@ -60,11 +60,11 @@ app.delete('/api/notes/:id', (req, res) => {
 
 // HTML Routes
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/notes.html'));
+  res.sendFile(path.join(__dirname, 'develop/public/notes.html'));
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
+  res.sendFile(path.join(__dirname, 'develop/public/index.html'));
 });
 
 app.listen(PORT, () => {
